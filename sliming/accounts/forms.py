06d648c923e2c669ed50b7568f43ee1c3e8ec796
lowelllll <yejin2651@naver.com>
2018-12-16 16:01:57 +0900

    [accounts] 가입시 개인정보 필드 수정
    이메일 제거, 출생년도 추가

--- sliming/accounts/forms.py
+++ sliming/accounts/forms.py
@@ -4,12 +4,21 @@ from .models import Profile
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('last_name', 'first_name', 'email')
+        fields = UserCreationForm.Meta.fields + ('last_name', 'first_name')
 
     profile_image = forms.ImageField(label='프로필 사진')
     nickname = forms.CharField(label='닉네임')
 
+    # CHOICES = (('Option 1', 'Option 1'), ('Option 2', 'Option 2'),)
+    CHOICES = ((str(year)+'-1-1 00:00:00', str(year)) for year in range(2018, 1969, -1))
+    birth_year = forms.ChoiceField(choices=tuple(CHOICES))
+
     def save(self):
         user = super().save()
-        profile= Profile.objects.create(user=user, profile_image=self.cleaned_data['profile_image'], nickname=self.cleaned_data['nickname'])
+        profile= Profile.objects.create(
+            user=user,
+            profile_image=self.cleaned_data['profile_image'],
+            nickname=self.cleaned_data['nickname'],
+            birth_year=self.cleaned_data['birth_year']
+        )
         return user
--- sliming/accounts/models.py
+++ sliming/accounts/models.py
@@ -5,6 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     nickname = models.CharField(max_length=30, null=True, blank=False)
     profile_image = models.ImageField(upload_to='%Y/%m/%d/profile')
+    birth_year = models.DateTimeField() # 출생년도 파악
 
     def __str__(self):
         return self.nickname
